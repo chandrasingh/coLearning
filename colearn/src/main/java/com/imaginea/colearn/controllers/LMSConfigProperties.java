cc0644b3088919c0ec775f732b2e66f7c8f0b18d
murthy-koppu <murthy.koppu@imaginea.com>
2013-08-22 18:19:56 +0530

    Corrected to load config.prop from class path

--- colearn/src/main/java/com/imaginea/colearn/controllers/LMSConfigProperties.java
+++ colearn/src/main/java/com/imaginea/colearn/controllers/LMSConfigProperties.java
@@ -1,21 +1,22 @@
 package com.imaginea.colearn.controllers;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class LMSConfigProperties extends Properties{
 	
 	public LMSConfigProperties() {
 		try {
-			this.load(new FileInputStream("config.properties"));
+			InputStream propFileInputStream = this.getClass().getClassLoader().getResourceAsStream("config.properties");			
+			this.load(propFileInputStream);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}
 	}
 }
